ec1e02078ff911d1e9d1bc85f3d21c94a03b9700
LissaGreense <walissa13@gmail.com>
2025-04-16 17:50:06 +0200

    ...

--- examples/googlecloud/googlecloud.go
+++ examples/googlecloud/googlecloud.go
@@ -37,7 +37,7 @@ func main() {
 	// Create and configure the agent
 	agentInstance, err := agent.New(
 		agent.WithServerAddr(":8080"),
-		agent.WithAPIKey("eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ4ZW4xNTduWG8wdjlLIiwiZXhwIjoxNzc1Nzg4MzI0LCJpc3MiOiJUZW1wZXN0IExhYnMsIEluYy4gKGxvY2FsKSIsImp0aSI6InBlbldkYTd5WDBROG4iLCJzdWIiOiJ2cDU1eXliQmVsbmRhIn0.AVf-nIhR0xzvv5NHSEq0Mg2NmrJuat5yX7ieE3FKNrx3hVIlSOlJDCtqVGGuP0sDuHHJ8KU3As9j3VllUX2KjDaYAK9pIuecQRE1YVQPg9TbOX20o1m_rtZ5qhdtGTFr5As2SjWFSrQNuTHpO7hPjyQ2DUWFKvvyXSieB84IBX_6Jv0e"),
+		agent.WithAPIKey("tempest-api-key"),
 		agent.WithAPIURL("http://localhost:8040"),
 		agent.WithWorkers(1),
 		agent.WithResultWorkers(1),
